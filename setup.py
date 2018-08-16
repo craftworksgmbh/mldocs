098ddd3bc80881f213a8b02f3f8a83310a80494d
si <simon.stiebellehner@craftworks.at>
2018-08-16 14:12:01 +0200

    changed setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages, setup
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='mldocs',
       version='0.0.1',
-      description='',
+      description="Documents the Machine Learning process from data to model and performance.",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Simon Stiebellehner',
       author_email='simon.stiebellehner@craftworks.at',
-      url='https://github.com/stiebels/mldocs',
+      url='https://github.com/craftworksgmbh/mldocs',
       packages=find_packages(exclude=['*test']),
       license='MIT',
       install_requires=[
@@ -15,4 +20,9 @@ setup(name='mldocs',
             'sklearn',
             'scipy'
       ],
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent'
+      ]
       )
